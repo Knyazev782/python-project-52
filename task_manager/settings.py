5ab88318a5cacb82ed7bf4cccd8550d20655186f
vipknyaz1488 <a.knyazev944@gmail.com>
2025-05-27 14:26:46 +0300

    Ошибка деплоя. Длинныое имя БД

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -79,6 +79,7 @@ WSGI_APPLICATION = 'hexlet_code.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600,
     )
 }
 
