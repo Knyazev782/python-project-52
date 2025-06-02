526010cd0e6f0c9fac035d4985affd1d26a463e7
vipknyaz1488 <a.knyazev944@gmail.com>
2025-06-02 16:19:50 +0300

    ошибки деплоя 5

---
+++ hexlet_code/urls.py
@@ -0,0 +1,32 @@
+"""
+URL configuration for hexlet_code project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from task_manager import views
+from django.contrib.auth.views import LoginView, LogoutView
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('users/', views.UserListView.as_view(), name='user_list'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('users/create/', views.CreateUserView.as_view(), name='create_users'),
+    path('users/<int:pk>/update/', views.UpdateUserView.as_view(), name='update_users'),
+    path('users/<int:pk>/delete/', views.DeleteUserView.as_view(), name='delete_users'),
+]
