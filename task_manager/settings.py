a3ea849e49db5931e0ac6da6b3ff7d095b6af2d1
vipknyaz1488 <a.knyazev944@gmail.com>
2025-06-04 13:22:35 +0300

    исправил ALLOWED_HOSTS = ["*"]

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+f(cw2+oo@*yd_%bi_r!_&0@=byo0w+=p4s4fpef_otp24^n52
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
