5bbb81ab4e6b00c9633c8c270716759854db6ac4
vipknyaz1488 <a.knyazev944@gmail.com>
2025-06-20 14:13:07 +0300

    исправииморт роллбар

--- task_manager/statuses/urls.py
+++ task_manager/statuses/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('create/', views.CreateStatus.as_view(), name='create_status'),
     path('<int:pk>/update/', views.UpdateStatus.as_view(), name='update_status'),
     path('<int:pk>/delete/', views.DeleteStatus.as_view(), name='delete_status'),
-    path('test-error/', views.test_error_view, name='test_error'),
-]
+#     path('test-error/', views.test_error_view, name='test_error'),
+# ]
