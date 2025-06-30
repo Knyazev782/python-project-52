79ce949ac4067b43f08abc4cde1e01b6b2cc9a42
vipknyaz1488 <a.knyazev944@gmail.com>
2025-06-30 12:43:16 +0300

    исправил воьюху и шаблон для входа

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -17,16 +17,13 @@ Including another URLconf
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from .views import IndexView
+from task_manager.users import views as users_views
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('users/', include('task_manager.users.urls')),
     path('statuses/', include('task_manager.statuses.urls')),
-    path('login/', auth_views.LoginView.as_view(
-        template_name='registration/login.html',
-        redirect_authenticated_user=True,
-        success_url='/'
-    ), name='login'),
+    path('login/', users_views.login_view, name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('tasks/', include('task_manager.tasks.urls')),
     path('labels/', include('task_manager.labels.urls'))
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -2,12 +2,13 @@ from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.shortcuts import redirect
-from django.db.models import ProtectedError
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib import messages
 from .models import Users
 from .forms import UserRegistrationForm
-from django.contrib import messages
-
+from django.contrib.auth.forms import AuthenticationForm
+from django.db.models import ProtectedError
 
 class UsersView(ListView):
     model = Users
@@ -17,14 +18,12 @@ class UsersView(ListView):
     def get_queryset(self):
         return Users.objects.all()
 
-
 class CreateUser(SuccessMessageMixin, CreateView):
     model = Users
     form_class = UserRegistrationForm
     template_name = 'users/user_create.html'
     success_url = reverse_lazy('login')
     success_message = 'Пользователь успешно зарегистрирован'
-
 
 class UpdateUser(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     model = Users
@@ -39,7 +38,6 @@ class UpdateUser(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
             messages.error(request, 'Вы не можете редактировать других пользователей')
             return redirect('users')
         return super().dispatch(request, *args, **kwargs)
-
 
 class DeleteUser(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
     model = Users
@@ -63,3 +61,17 @@ class DeleteUser(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
         except ProtectedError:
             messages.error(request, 'Нельзя удалить пользователя, потому что он используется')
             return redirect(self.success_url)
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, "Неверное имя пользователя или пароль.")
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
